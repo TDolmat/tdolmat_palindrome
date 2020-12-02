c35593294c1bedf9a154279e710070ca220053c3
Tomasz Dołmat <tomasz.dolmat@gmail.com>
2020-12-02 15:03:31 +0100

    Finished palindrome code

--- lib/tdolmat_palindrome.rb
+++ lib/tdolmat_palindrome.rb
@@ -1,6 +1,16 @@
 require "tdolmat_palindrome/version"
 
-module TdolmatPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
